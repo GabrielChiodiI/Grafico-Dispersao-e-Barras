ad70c50c450d6e0e0ae57c95824af7376cc80f64
GabrielChiodiI <gabrielmju@hotmail.com>
2024-10-10 13:58:14 -0400

    codigo inicial, vizualização da comunidade

---
+++ src/localMessage.js
@@ -0,0 +1,443 @@
+export const message = {
+  "tables": {
+    "DEFAULT": [
+      {
+        "dimension": [
+          "20240915"
+        ],
+        "metric": [
+          50.9
+        ]
+      },
+      {
+        "dimension": [
+          "20240920"
+        ],
+        "metric": [
+          25.8
+        ]
+      },
+      {
+        "dimension": [
+          "20240914"
+        ],
+        "metric": [
+          3.1
+        ]
+      },
+      {
+        "dimension": [
+          "20240926"
+        ],
+        "metric": [
+          2.9
+        ]
+      },
+      {
+        "dimension": [
+          "20240921"
+        ],
+        "metric": [
+          0.9
+        ]
+      },
+      {
+        "dimension": [
+          "20240911"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240917"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240918"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240919"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240916"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240913"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240922"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240923"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240924"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240925"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240912"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240927"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240928"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240929"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20240930"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20241001"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20241002"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20241003"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20241004"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20241005"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20241006"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20241007"
+        ],
+        "metric": [
+          0
+        ]
+      },
+      {
+        "dimension": [
+          "20241008"
+        ],
+        "metric": [
+          0
+        ]
+      }
+    ]
+  },
+  "dateRanges": {
+    "DEFAULT": {
+      "start": "20240911",
+      "end": "20241008"
+    }
+  },
+  "fields": {
+    "dimension": [
+      {
+        "id": "qt_5ypvi4fuld",
+        "name": "Data",
+        "type": "YEAR_MONTH_DAY",
+        "concept": "DIMENSION"
+      }
+    ],
+    "metric": [
+      {
+        "id": "qt_qnnvi4fuld",
+        "name": "Precipitação",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ]
+  },
+  "style": {
+    "barColor": {
+      "value": {
+        "color": "#1e555c"
+      },
+      "defaultValue": {
+        "color": "#1e555c"
+      }
+    }
+  },
+  "theme": {
+    "themeFillColor": {
+      "color": "#ffffff",
+      "themeRef": {
+        "index": 0
+      }
+    },
+    "themeFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 1
+      }
+    },
+    "themeFontFamily": "Roboto",
+    "themeAccentFillColor": {
+      "color": "#e0e0e0",
+      "themeRef": {
+        "index": 2
+      }
+    },
+    "themeAccentFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 3
+      }
+    },
+    "themeAccentFontFamily": "Roboto",
+    "themeSeriesColor": [
+      {
+        "color": "#0072f0",
+        "seriesRef": {
+          "index": 0
+        },
+        "themeRef": {
+          "index": 4
+        }
+      },
+      {
+        "color": "#00b6cb",
+        "seriesRef": {
+          "index": 1
+        },
+        "themeRef": {
+          "index": 5
+        }
+      },
+      {
+        "color": "#f10096",
+        "seriesRef": {
+          "index": 2
+        },
+        "themeRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#f66d00",
+        "seriesRef": {
+          "index": 3
+        },
+        "themeRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ffa800",
+        "seriesRef": {
+          "index": 4
+        },
+        "themeRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#7cb342",
+        "seriesRef": {
+          "index": 5
+        },
+        "themeRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#5e35b1",
+        "seriesRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#03a9f4",
+        "seriesRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ec407a",
+        "seriesRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#ff7043",
+        "seriesRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#737373",
+        "seriesRef": {
+          "index": 10
+        }
+      },
+      {
+        "color": "#f15a60",
+        "seriesRef": {
+          "index": 11
+        }
+      },
+      {
+        "color": "#7ac36a",
+        "seriesRef": {
+          "index": 12
+        }
+      },
+      {
+        "color": "#5a9bd4",
+        "seriesRef": {
+          "index": 13
+        }
+      },
+      {
+        "color": "#faa75a",
+        "seriesRef": {
+          "index": 14
+        }
+      },
+      {
+        "color": "#9e67ab",
+        "seriesRef": {
+          "index": 15
+        }
+      },
+      {
+        "color": "#ce7058",
+        "seriesRef": {
+          "index": 16
+        }
+      },
+      {
+        "color": "#d77fb3",
+        "seriesRef": {
+          "index": 17
+        }
+      },
+      {
+        "color": "#81d4fa",
+        "seriesRef": {
+          "index": 18
+        }
+      },
+      {
+        "color": "#f48fb1",
+        "seriesRef": {
+          "index": 19
+        }
+      }
+    ],
+    "themeIncreaseColor": {
+      "color": "#388e3c"
+    },
+    "themeDecreaseColor": {
+      "color": "#f44336"
+    },
+    "themeGridColor": {
+      "color": "#d1d1d1"
+    }
+  },
+  "interactions": {},
+  "colorMap": {}
+};
